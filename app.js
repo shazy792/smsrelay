d534537e047603954d8e8683b3fc4770880635b9
Shahzil Sheikh <shazy792@hotmail.com>
2017-03-30 17:26:43 -0500

    changed port

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('Relay started on port 3000!')
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Relay started')
 })
