58374dd5e2cc4ba25e39b40b55df522d33a56ebf
Shahzil Sheikh <shazy792@hotmail.com>
2017-03-30 17:41:45 -0500

    Added twilio + security

--- app.js
+++ app.js
@@ -2,11 +2,26 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
 
+var accountSid = 'ACa93dc2d648868bd6e31a21099a277dbc'; 
+var authToken = process.env.AUTH;
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/:ph/:text/:sec', function (req, res) {
+  //require the Twilio module and create a REST client 
+    var client = require('twilio')(accountSid, authToken); 
+
+    if (req.params.sec === process.env.sec) {
+        client.messages.create({ 
+            to: req.params.ph, 
+            from: "+14043818013", 
+            body: req.params.text, 
+        }, function(err, message) { 
+            console.log("Message Sent"); 
+        });
+    }    
+
 })
 
 app.listen(process.env.PORT || 3000, function () {
